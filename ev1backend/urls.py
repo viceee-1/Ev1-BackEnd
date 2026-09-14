2b138c33161a6fc5b1750a742141700c2a1fd809
viceee-1 <in.cabrera1010@gmail.com>
2026-09-14 15:07:56 -0300

    ev1backend/urls.py modificado, inicio.html modificado

--- ev1backend/urls.py
+++ ev1backend/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('inicio.urls')),
+    path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
     path('app2/', include('app2.urls')),
 ]
