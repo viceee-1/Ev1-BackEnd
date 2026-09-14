0136958038d67e0f345a94377b817afc63bc0532
viceee-1 <in.cabrera1010@gmail.com>
2026-09-14 14:44:49 -0300

    app1, urls, vista1_app1 y vista2_app1 creadas

--- ev1backend/urls.py
+++ ev1backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('inicio.urls')),
     path('admin/', admin.site.urls),
+    path('', include('inicio.urls')),
+    path('app1/', include('app1.urls')),
 ]
